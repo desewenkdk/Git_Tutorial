6ce7858a01fc9241130b5706c51bcebd01fcf65d
desewenkdk <171018since400@gmail.com>
2018-08-07 14:50:41 +0900

    sum() 추가

--- calculator.java
+++ calculator.java
@@ -4,4 +4,8 @@ public class Calculator{
     public static void main(String[] args){
         System.out.println("Hello world")
     }
+
+    public int sum(int a, int b){
+      return a + b;
+    }
 }
